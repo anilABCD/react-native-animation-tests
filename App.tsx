ef16750342b654c0b434c422472bce8222f53ef5
Anil <786.anil.potlapally@gmail.com>
2022-10-16 21:01:47 +0530

    Interpolation ADDED

--- App.tsx
+++ App.tsx
@@ -6,11 +6,12 @@ import Translate from "./AnimationsILearnedAndWrote/Translate";
 import Flipping from "./AnimationsILearnedAndWrote/Flipping";
 import Scale from "./AnimationsILearnedAndWrote/Scale";
 import HeightAndWidth from "./AnimationsILearnedAndWrote/HeightAndWidth";
+import Interpolation from "./AnimationsILearnedAndWrote/Interpolation";
 
 export default function App() {
   return (
     <SafeAreaView>
-      <HeightAndWidth />
+      <Interpolation />
     </SafeAreaView>
   );
 }
